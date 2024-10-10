199e5022cc23b9c4122c4caea28a2d68f4fc79d6
yood2 <yood2@proton.me>
2024-10-09 21:39:10 -0700

    updated font size, added text to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Hello world!</p>
-    </div>
-  );
+    return (
+        <div className="h-full flex justify-center items-center p-4 sm:p-6 md:p-8 lg:p-10">
+            <div className="flex flex-col space-y-6 max-w-2xl w-full">
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                    Hi, I&apos;m Daniel!
+                </h3>
+                <p className="text-base sm:text-lg">
+                    I am a student at the University of British Columbia doing a
+                    combined major in Business and Computer Science. I like
+                    programming, chess, running, weightlifting, and coffee.
+                    Currently fixated on Python internals and optimization.
+                </p>
+            </div>
+        </div>
+    );
 }
