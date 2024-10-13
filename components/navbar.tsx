fd967d43cc9e6dc709de64eea08f15b295de85b9
yood2 <yood2@proton.me>
2024-10-13 15:10:53 -0700

    removed unfinished route

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,17 @@
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import Link from 'next/link';
 
 export default function Navbar() {
     return (
         <nav className="w-full">
             <div className="flex justify-start items-center space-x-4 py-2">
                 <Button variant="link" className="px-2">
-                    <a href="/">About Me</a>
+                    <Link href="/">Home</Link>
                 </Button>
-                <Button variant="link" className="px-2">
+                {/* <Button variant="link" className="px-2">
                     <a href="/coffee">Coffee Journal</a>
-                </Button>
+                </Button> */}
             </div>
             <Separator />
         </nav>
