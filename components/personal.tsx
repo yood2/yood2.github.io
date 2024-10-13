7f5851d0f44f410aece6df7ba904b753916ea61e
yood2 <yood2@proton.me>
2024-10-13 12:33:33 -0700

    more changes

--- components/personal.tsx
+++ components/personal.tsx
@@ -1,30 +1,19 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-
-const Arrow = () => (
-    <svg
-        width="15"
-        height="15"
-        viewBox="0 0 15 15"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path d="M6 11L6 4L10.5 7.5L6 11Z" fill="currentColor"></path>
-    </svg>
-);
+import LinkIcon from './LinkIcon';
 
 export default function Personal() {
     return (
         <div className="flex flex-col space-y-0 py-4">
             <Link href="https://www.linkedin.com/in/yood2/">
                 <Button variant="link" className="px-0 justify-start">
-                    <Arrow />
+                    <LinkIcon />
                     LinkedIn
                 </Button>
             </Link>
             <Link href="https://github.com/yood2">
                 <Button variant="link" className="px-0 justify-start">
-                    <Arrow />
+                    <LinkIcon />
                     GitHub
                 </Button>
             </Link>
