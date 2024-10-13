557999d8e27300e18775da51d01bf95a32fda809
yood2 <yood2@proton.me>
2024-10-13 13:58:17 -0700

    fixed links

--- components/personal.tsx
+++ components/personal.tsx
@@ -5,18 +5,22 @@ import LinkIcon from './LinkIcon';
 export default function Personal() {
     return (
         <div className="flex flex-col space-y-0 py-4">
-            <Link href="https://www.linkedin.com/in/yood2/">
-                <Button variant="link" className="px-0 justify-start">
-                    <LinkIcon />
-                    LinkedIn
-                </Button>
-            </Link>
-            <Link href="https://github.com/yood2">
-                <Button variant="link" className="px-0 justify-start">
-                    <LinkIcon />
-                    GitHub
-                </Button>
-            </Link>
+            <div>
+                <Link href="https://www.linkedin.com/in/yood2/">
+                    <Button variant="link" className="px-0 justify-start">
+                        <LinkIcon />
+                        LinkedIn
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <Link href="https://github.com/yood2">
+                    <Button variant="link" className="px-0 justify-start">
+                        <LinkIcon />
+                        GitHub
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
