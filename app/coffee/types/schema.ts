e0cf15fdbc90e0f2b4b12d2120958c6d7626f5aa
yood2 <yood2@proton.me>
2025-02-28 11:06:14 -0800

    edited project description for moss

--- app/coffee/types/schema.ts
+++
@@ -1,4 +0,0 @@
-export interface Recipe {
-    title: string;
-    ratio: string;
-}
--- data/project-data.ts
+++ data/project-data.ts
@@ -1,9 +1,23 @@
 import { ProjectProps } from '@/components/project';
 
 export const ProjectList: ProjectProps[] = [
+    // {
+    //     title: 'Neuro-Signal Visualizer',
+    //     desc: 'Created a data visualization component for the UBC Multifaceted Innovations in NeuroTechnology design team. Can receive signals at 256Hz and render EEG data with custom settings.',
+    //     tags: [
+    //         'Typescript',
+    //         'React',
+    //         'Next.js',
+    //         'TailwindCSS',
+    //         'ReCharts',
+    //         'Shadcn/ui',
+    //     ],
+    //     source: 'https://github.com/yood2/frontend-product-simulated-viz',
+    //     demo: '/neuro',
+    // },
     {
-        title: 'Neuro-Signal Visualizer',
-        desc: 'Created a data visualization component for the UBC Multifaceted Innovations in NeuroTechnology design team. Can receive signals at 256Hz and render EEG data with custom settings.',
+        title: 'MINT Open Source System',
+        desc: 'Creating a open source Brain-Computer Interface for collecting and managing brain waves for the UBC Multifacted Innovations in Neuro-Tech team.',
         tags: [
             'Typescript',
             'React',
@@ -13,7 +27,7 @@ export const ProjectList: ProjectProps[] = [
             'Shadcn/ui',
         ],
         source: 'https://github.com/yood2/frontend-product-simulated-viz',
-        demo: '/neuro',
+        demo: '/moss',
     },
     {
         title: 'Data Cork',
